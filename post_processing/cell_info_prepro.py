b92eb5ccee93f0861966c4c04ab624504314ba2e
Jing You <ken32293355@gmail.com>
2022-01-13 20:07:15 +0800

    modify column

--- post_processing/cell_info_prepro.py
+++ post_processing/cell_info_prepro.py
@@ -1,41 +1,27 @@
 import pandas as pd
 
 
-fname = "083021063811.csv"
-names = ["Date",
-"RxRate",
-"TxRate",
-"DLBandwidth",
-"ULBandwidth",
-"LTE",
-"LTE_RSRP",
-"LTE_RSRQ",
-"NR",
-"NR_SSRSRP",
-"NR_SSRSRQ",
-"NR_CSIRSRP",
-"NR_CSIRSRQ",
-"Type",
-"MNC",
-"MCC",
-"CID",
-"PCI",
-"SigStrength",
-"earfcn"]
+fname = r"C:\Users\User\Desktop\UEdata\0\cellinfo0.csv"
+names = ["Date","GPSLat","GPSLon","GPSSpeed","RxRate","TxRate","DLBandwidth","ULBandwidth","MNC","MCC","CID","PCI","LTE_RSRP","LTE_RSRQ","NR_SSRSRP","NR_SSRSRQ","earfcn","PCI1","LTE_RSRP1","LTE_RSRQ1","earfcn1","PCI2","LTE_RSRP2","LTE_RSRQ2","earfcn2","PCI3","LTE_RSRP3","LTE_RSRQ3","earfcn3","PCI4","LTE_RSRP4","LTE_RSRQ4","earfcn4","PCI5","LTE_RSRP5","LTE_RSRQ5","earfcn5","PCI6","LTE_RSRP6","LTE_RSRQ6","earfcn6","PCI7","LTE_RSRP7","LTE_RSRQ7","earfcn7","PCI8","LTE_RSRP8","LTE_RSRQ8","earfcn8","PCI9","LTE_RSRP9","LTE_RSRQ9","earfcn9","PCI10","LTE_RSRP10","LTE_RSRQ10",]
+f = open(fname, "r")
+strlist = f.readlines()
+f.close()
+if strlist[0][:4] == "Date":
+    strlist[0] = ",".join(names)
 
-df = pd.read_csv(fname, names=names)
+f = open(fname, "w")
 
-def to_zero(x):
-    return x if x != '-' else 0
+f.write("".join(strlist))
 
-
-df.loc[:, "NR_SSRSRP"] = df.loc[:, "NR_SSRSRP"].apply(to_zero)
-df.loc[:, "NR_SSRSRQ"] = df.loc[:, "NR_SSRSRQ"].apply(to_zero)
-df.loc[:, "NR_CSIRSRP"] = df.loc[:, "NR_CSIRSRP"].apply(to_zero)
-df.loc[:, "NR_CSIRSRQ"] = df.loc[:, "NR_CSIRSRQ"].apply(to_zero)
-
-df.to_csv(fname+"2.csv", index=False)
-# df.to_csv()
-
-print(df)
+# df = pd.read_csv(fname, names=names)
+# print(df)
+# def to_zero(x):
+#     return x if x != '-' else 0
+# # df.loc[:, "NR_SSRSRP"] = df.loc[:, "NR_SSRSRP"].apply(to_zero)
+# # df.loc[:, "NR_SSRSRQ"] = df.loc[:, "NR_SSRSRQ"].apply(to_zero)
+# # df.loc[:, "NR_CSIRSRP"] = df.loc[:, "NR_CSIRSRP"].apply(to_zero)
+# # df.loc[:, "NR_CSIRSRQ"] = df.loc[:, "NR_CSIRSRQ"].apply(to_zero)
+# # df.to_csv(fname+"2.csv", index=False)
+# # # df.to_csv()
+# # print(df)
 
