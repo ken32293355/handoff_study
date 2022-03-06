b702983a9cc903a82627996f33e77d7b543e794a
ltshandsome <ltshandsome@hotmail.com.tw>
2022-03-06 18:05:23 +0800

    remove os.system

--- socket_program/seperate_DL2_server.py
+++ socket_program/seperate_DL2_server.py
@@ -1,42 +1,49 @@
 #!/usr/bin/env python3
-
-# from asyncio import subprocess
+# -*- coding: utf-8 -*-
+from calendar import c
 import socket
 import time
 import threading
-import os
 import datetime as dt
-import argparse
+import select
+import sys
+import os
+import queue
 import subprocess
+import argparse
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", type=int,
                     help="port to bind", default=3237)
 args = parser.parse_args()
-print(args.port)
-
 
 
 HOST = '192.168.1.248'
-PORT = args.port
+PORT = 3237
+PORT2 = 3238
 thread_stop = False
 exit_program = False
 length_packet = 250
-bandwidth = 2000*100
-total_time = 3600
+bandwidth = 20000*1000
+total_time = 10
 expected_packet_per_sec = bandwidth / (length_packet << 3)
 sleeptime = 1.0 / expected_packet_per_sec
 prev_sleeptime = sleeptime
+server_addr = (HOST, PORT)
+thread_stop = False
+exitprogram = False
 pcap_path = "pcapdir"
-
 hostname = str(PORT) + ":"
 
-def connection():
+def connection(host, port, result):
+    assert(result[0] == None)
+    hostname = str(port) + ":"
     s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s_tcp.bind((HOST, PORT))
-    s_udp.bind((HOST, PORT))
+    s_tcp.bind((host, port))
+    s_udp.bind((host, port))
 
     udp_addr = ""
 
@@ -93,14 +100,13 @@ def connection():
     except:
         exit(1)
 
+    result[0] = s_tcp, s_udp, conn, tcp_addr, udp_addr 
 
-    return s_tcp, s_udp, conn, tcp_addr, udp_addr
 
 def remote_control(conn, t):
     global thread_stop
     global exit_program
-    
-    while t.is_alive() and not thread_stop:
+    while t.is_alive():
         try:
             print("waiting for stopping")
             indata, addr = conn.recvfrom(1024)
@@ -116,8 +122,8 @@ def remote_control(conn, t):
             print("Error: ", inst)
     print("STOP remote control")
 
-def transmision(s_udp, conn, udp_addr):
-    print("start transmision to addr", udp_addr)
+def transmision(s_udp1, s_udp2, udp_addr1, udp_addr2):
+    print("start transmision to addr:%s and addr:%s"%(udp_addr1, udp_addr2))
     i = 0
     prev_transmit = 0
     ok = (1).to_bytes(1, 'big')
@@ -132,11 +138,12 @@ def transmision(s_udp, conn, udp_addr):
         z = i.to_bytes(8, 'big')
         redundent = os.urandom(250-8*3-1)
         outdata = datetimedec.to_bytes(8, 'big') + microsec.to_bytes(8, 'big') + z + ok +redundent
-        s_udp.sendto(outdata, udp_addr)
+        s_udp1.sendto(outdata, udp_addr1)
+        s_udp2.sendto(outdata, udp_addr2)
         i += 1
         time.sleep(sleeptime)
         if time.time()-start_time > count:
-            #print("[%d-%d]"%(count-1, count), "transmit", i-prev_transmit)
+            print("[%d-%d]"%(count-1, count), "transmit", i-prev_transmit)
             count += 1
             sleeptime = prev_sleeptime / expected_packet_per_sec * (i-prev_transmit) # adjust sleep time dynamically
             prev_transmit = i
@@ -148,92 +155,52 @@ def transmision(s_udp, conn, udp_addr):
     ok = (0).to_bytes(1, 'big')
     redundent = os.urandom(250-8*3-1)
     outdata = datetimedec.to_bytes(8, 'big') + microsec.to_bytes(8, 'big') + z + ok +redundent
-    s_udp.sendto(outdata, udp_addr)
-
+    s_udp1.sendto(outdata, udp_addr1)
+    s_udp2.sendto(outdata, udp_addr2)
+    thread_stop = True
     print("transmit", i, "packets")
 
 
-def bybass_rx(s_udp):
-    s_udp.settimeout(3)
-    print("wait for indata...")
-    i = 0
-    start_time = time.time()
-    count = 1
-    seq = 0
-    prev_capture = 0
-    prev_loss = 0
-    global thread_stop
-    while not thread_stop:
-        try:
-            indata, addr = s_udp.recvfrom(1024)
-            if len(indata) != 250:
-                print("WTF len", len(indata))
-            seq = int(indata[16:24].hex(), 16)
-            ts = int(int(indata[0:8].hex(), 16)) + float("0." + str(int(indata[8:16].hex(), 16)))
-            # print(dt.datetime.fromtimestamp(time.time())-dt.datetime.fromtimestamp(ts)-dt.timedelta(seconds=0.28))
-            # s_local.sendall(indata)
-            ok = int(indata[24:25].hex(), 16)
-            # buffer.put(indata)
-            if ok == 0:
-                break
-            else:
-                i += 1
-            if time.time()-start_time > count:
-                #print("[%d-%d]"%(count-1, count), "capture", i-prev_capture, "loss", seq-i+1-prev_loss, sep='\t')
-                prev_loss += seq-i+1-prev_loss
-                count += 1
-                prev_capture = i
-        except Exception as inst:
-            print("Error: ", inst)
-            thread_stop = True
-    thread_stop = True
-    print("[%d-%d]"%(count-1, count), "capture", i-prev_capture, "loss", seq-i+1-prev_loss, sep='\t')
-    print("---Experiment Complete---")
-    print("Total capture: ", i, "Total lose: ", seq - i + 1)
-    print("STOP bypass")
 
-
-if not os.path.exists(pcap_path):
-    os.system("mkdir %s"%(pcap_path))
-
-
-# os.system("kill -9 $(ps -A | grep python | awk '{print $1}')") 
-
-while not exit_program:
+while not exitprogram:
 
     now = dt.datetime.today()
     n = '-'.join([str(x) for x in[ now.year, now.month, now.day, now.hour, now.minute, now.second]])
-    #os.system("echo wmnlab | sudo -S pkill tcpdump")
-    # os.system("echo wmnlab | sudo -S tcpdump -i any port %s -w %s/%s_%s.pcap&"%(PORT, pcap_path,PORT, n))
-    tcpproc =  subprocess.Popen(["tcpdump -i any port %s -w %s/%s_%s.pcap&"%(PORT, pcap_path,PORT, n)], shell=True)
+    tcpproc1 =  subprocess.Popen(["tcpdump -i any port %s -w %s/%s_%s.pcap&"%(PORT, pcap_path,PORT, n)], shell=True)
+    tcpproc2 =  subprocess.Popen(["tcpdump -i any port %s -w %s/%s_%s.pcap&"%(PORT2, pcap_path,PORT2, n)], shell=True)
     time.sleep(2)
+    # s_tcp1, s_udp1, conn1, tcp_addr1, udp_addr1 = connection()
+    # s_tcp2, s_udp2, conn2, tcp_addr2, udp_addr2 = connection()
+    result1 = [None]
+    result2 = [None]
+    connection_t1 = threading.Thread(target = connection, args = (HOST, PORT, result1))
+    connection_t2 = threading.Thread(target = connection, args = (HOST, PORT2, result2))
     try:
-        s_tcp, s_udp, conn, tcp_addr, udp_addr = connection()
+        connection_t1.start()
+        connection_t2.start()
+        connection_t1.join()
+        connection_t2.join()
+        s_tcp1, s_udp1, conn1, tcp_addr1, udp_addr1 = result1
+        s_tcp2, s_udp2, conn2, tcp_addr2, udp_addr2 = result2
     except Exception as inst:
-        print("Connection Error:", inst)
+        print("Error: ", inst)
+        tcpproc1.terminate()
+        tcpproc2.terminate()
         continue
+    print("connect two UE suceed")
     thread_stop = False
-    t = threading.Thread(target = transmision, args = (s_udp, conn, udp_addr))
-    t1 = threading.Thread(target = bybass_rx, args = (s_udp,))
-    t2 = threading.Thread(target = remote_control, args = (conn, t))
-
-    t.start()
-    t1.start()
+    t4 = threading.Thread(target = transmision, args = (s_udp1, s_udp2, udp_addr1, udp_addr2))
+    t2 = threading.Thread(target = remote_control, args = (conn1, t4))
     t2.start()
+    t4.start()
     
-    t.join()
-    t1.join()
     t2.join()
-    s_tcp.close()
+    t4.join()
+    s_tcp1.close()
+    s_udp1.close()
+    s_udp2.close()
+    conn1.close()
+    conn2.close()
+    tcpproc1.terminate()
+    tcpproc2.terminate()
 
-    s_udp.settimeout(0)
-    while True:
-        try:
-            print("reading trash...")
-            indata, udp_addr = s_udp.recvfrom(1024)
-        except:
-            break
-    s_udp.close()
-    conn.close()
-    # os.system("echo wmnlab | sudo -S pkill tcpdump")
-    tcpproc.terminate()
