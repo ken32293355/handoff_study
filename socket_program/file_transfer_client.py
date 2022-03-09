66a23100d86befd6e45226ee1267c77eef61e8ee
Jing You <ken32293355@gmail.com>
2022-03-09 14:55:51 +0800

    add file

---
+++ socket_program/file_transfer_client.py
@@ -0,0 +1,48 @@
+from fileinput import filename
+import socket
+import time
+import os
+import time
+
+TCP_IP = '140.112.20.183'
+TCP_PORT = 3280
+BUFFER_SIZE = 1024
+
+devicename = open("device.txt", "r").readline()
+devicename = devicename if '\n' not in devicename else devicename[:-1]
+
+target_dir1 = r"/sdcard/handoff_study/socket_program/pcapdir"
+target_dir2 = r"/sdcard/Android/data/com.example.cellinfomonitor/files/Documents"
+
+def senddir(target_dir):
+
+    dirlist = os.listdir(target_dir)
+
+    for filename in dirlist:
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP, TCP_PORT))
+        print("devicename", devicename)
+        print("filename", filename)
+        s.sendall(devicename.encode())
+        time.sleep(0.5)
+        s.sendall(filename.encode())
+        time.sleep(0.5)
+        msg = s.recv(65535)
+        if msg == b'NO':
+            s.close()
+            print("PASS", filename)
+            continue
+        elif msg == b'OK':
+            print("start to send file...")
+        f = open(os.path.join(target_dir, filename), 'rb')
+
+        s.sendfile(f)
+        f.close()
+        s.close()
+        print('Successfully send the file', filename)
+        print('connection closed')
+        time.sleep(1)
+
+senddir(target_dir2)
+senddir(target_dir1)
