6d948362028e63f66a34379cf55d2bc93b2d20d4
JingYou <ken32293355@gmail.com>
2022-04-23 20:47:36 +0800

    add argv

--- post_processing/tcp_pcap_to_csv.py
+++ post_processing/tcp_pcap_to_csv.py
@@ -2,21 +2,21 @@ from operator import sub
 import subprocess
 import os
 import sys
-
+import time
 def pcap_to_csv(infilepath, outfilepath):
     s = "tshark -r %s -T\
 fields -e frame.number -e frame.time -e ip.src -e ip.dst\
- -e frame.len -e tcp.analysis.acks_frame  -e tcp.len -e tcp.analysis.ack_rtt  -e _ws.col.Info -e tcp.payload -E header=y -E separator=@ >%s"%(infilepath, outfilepath)
-
+ -e frame.len -e tcp.analysis.acks_frame  -e tcp.len -e tcp.analysis.ack_rtt -e tcp.srcport -e tcp.dstport -e _ws.col.Info -e tcp.payload -E header=y -E separator=@ >%s"%(infilepath, outfilepath)
     print(s)
     subprocess.Popen([s], shell=True)
+    time.sleep(60)
 
+dirname = sys.argv[1]
 
-filename = "/home/wmnlab/D/xm14/2022-4-6-18-34-48.pcap"
-pcap_to_csv(filename, "/home/wmnlab/D/pcap_csv/" + "UE_0406.csv")
+filenames = os.listdir(dirname)
 
-filename = "/home/wmnlab/D/pcap_data2/DL_3257_2022-4-6-18-19-55.pcap"
-pcap_to_csv(filename, "/home/wmnlab/D/pcap_csv/" + "DL_0406.csv")
+for fname in sorted(filenames):
+    if not fname.endswith(".pcap"):
+        continue
 
-filename = "/home/wmnlab/D/pcap_data2/UL_3267_2022-4-6-18-19-55.pcap"
-pcap_to_csv(filename, "/home/wmnlab/D/pcap_csv/" + "UL_0406.csv")
+    pcap_to_csv(os.path.join(dirname, fname), os.path.join(dirname, fname[:fname.find(".pcap")]+"_pcap.csv"))
